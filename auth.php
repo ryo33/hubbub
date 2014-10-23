94f4ff1a629ff05d86f755f3f6b08a70a999ab0e
ryo33 <ryo12redstone@gmail.com>
2014-10-23 05:20:42 -0700

    get can connect

--- auth.php
+++ auth.php
@@ -1,9 +1,11 @@
-
+<!DOCTYPE html>
+<html>
+<body>
 <?php
 include "setting.php";
 
-$url = 'http://github.com/login/oauth/access_token';
-$request = array('client_id' => $client_id, 'client_secret' => $client_secret, 'code' => $_POST['code']);
+$url = 'https://github.com/login/oauth/access_token';
+$request = array('client_id' => $client_id, 'client_secret' => $client_secret, 'code' => $_GET['code']);
 
 $options = array(
     'http' => array(
@@ -15,9 +17,12 @@ $options = array(
 $con = stream_context_create($options);
 $res = file_get_contents($url, false, $con);
 
-var_dump($res);
+$token = preg_match("/access_token=(\w*)/", $res);
+echo $token;
 ?>
 <script>
 localStorage.setItem("token", <?php echo $token;?>);
-$(location).attr("href", "http://hubbub.com");
+location.href = "http://hubbub.com";
 </script>
+</body>
+</html>
